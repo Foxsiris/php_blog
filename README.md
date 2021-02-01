987f1b527b7ac470a3765d39082ccddd044d99ce
Daniil <daniilfgs@gmail.com>
2021-02-01 18:35:29 +0400

    Покдлючил с помощью Composer (Slim , Twig) , настроили маршрутизацию и шаблонизатор.

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # php_blog
+Этапы запуска этого приложения (памятка как все это запустить)
+1)установка Composer он должен работать
+2)Composer init
+3)Команды для установки фраймворека Slim 
+    a)composer require slim/slim:"4.*"
+    b)composer require slim/psr7
+4) Создание файла .htaccess 
+    в него внести данные: RewriteEngine On
+                         RewriteCond %{REQUEST_FILENAME} !-f
+                         RewriteCond %{REQUEST_FILENAME} !-d
+                         RewriteRule ^ index.php [QSA,L]
+   
---
+++ index.php
@@ -0,0 +1,27 @@
+<?php
+use Psr\Http\Message\ResponseInterface as Response;
+use Psr\Http\Message\ServerRequestInterface as Request;
+use Slim\Factory\AppFactory;
+
+require __DIR__ . '/vendor/autoload.php';
+
+$loader = new \Twig\Loader\FilesystemLoader('templates'); // используется Twig папка с шаблонами
+$view = new \Twig\Environment($loader);
+
+$app = AppFactory::create();
+
+$app->get('/', function (Request $request, Response $response, $args) use($view){
+    $body= $view->render('index.twig');
+    $response->getBody()->write($body);
+    return $response;
+});
+
+$app->get('/about', function (Request $request, Response $response, $args) use($view){
+    $body=$view->render('about.twig',[
+        'name'=> 'Daniil'
+    ]);// выбираем шаблон и вторым  аргументом передаем параметры
+    $response->getBody()->write($body);
+    return $response;
+});
+
+$app->run();
